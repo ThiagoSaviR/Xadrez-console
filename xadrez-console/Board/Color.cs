de2ae10e45a79de6ab1c14d5ac4d8930d783a716
Thiago Ribeiro <thiagosavir@gmail.com>
2022-11-25 15:58:06 -0300

    feature: Init chess board

---
+++ xadrez-console/Board/Color.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board
+{
+    enum Color
+    {
+        White,
+        Black,
+        Yellow,
+        Blue,
+        Green,
+        Orange
+    }
+}
---
+++ xadrez-console/Board/Piece.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board
+{
+    class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int MoveQtt { get; protected set; }
+        public ChessBoard Board { get; protected set; }
+
+        public Piece(Position position, Color color, ChessBoard board)
+        {
+            Position = position;
+            Color = color;
+            MoveQtt = 0;
+            Board = board;
+        }
+    }
+
+}
